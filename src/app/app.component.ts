4aef44ca414bfcdadbc4ad1fb6b46a4ca311dd41
Paulo <pauloafpjunior@hotmail.com>
2021-10-01 17:22:44 -0300

    Add toast

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
@@ -6,16 +7,36 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(private toastCtrl: ToastController) { }
 
   error = true;
 
   items: any[] = [
-    {name: "Pampers", price: 45, quantity: 40},
-    {name: "Pompo", price: 47, quantity: 38}
+    { name: "Pampers", price: 45, quantity: 40 },
+    { name: "Pompo", price: 47, quantity: 38 }
   ]
 
   get theBestOne() {
     return this.items.length > 0 ? this.items[0] : null;
+  }
+
+  async onAdd() {
+    const previousToast = await this.toastCtrl.getTop();
+    if (previousToast) {
+      await this.toastCtrl.dismiss();
+    }
+
+    const toast = await this.toastCtrl.create(
+      {
+        message: 'Adicionado!',
+        duration: 2000,
+        buttons: [
+          {
+            text: 'Fechar'
+          }
+        ]
+      }
+    );
+    toast.present();
   }
 }
